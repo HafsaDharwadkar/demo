897d77401b592869110a47faa86c79080fb21947
ADMIN <ADMIN@DESKTOP-68C1C7O>
2023-04-13 11:51:58 +0530

    framework dev

---
+++ src/test/java/org/demo3.java
@@ -0,0 +1,11 @@
+package org;
+
+public class demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("demo");
+		System.out.println("this from frame work devloper");
+	}
+
+}
